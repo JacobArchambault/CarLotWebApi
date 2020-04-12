9443204975cc51a5db73c59bf80a1c48f4bf0e1d
Jacob Archambault <jacob.archambault@waystar.com>
2020-04-12 09:06:43 -0400

    Add attribute routing to inventory controller.

--- CarLotWebApi/Controllers/InventoryController.cs
+++ CarLotWebApi/Controllers/InventoryController.cs
@@ -9,7 +9,12 @@ using AutoLotDAL.Repos;
 using AutoMapper;
 namespace CarLotWebApi.Controllers
 {
+    [Route("api/Inventory/{id?}")]
     public class InventoryController : ApiController
     {
+        [HttpGet, Route("")]
+        public IEnumerable<string> Get() => new string[] { "value1", "value2" };
+        [HttpGet, Route("{id}")]
+        public string Get(int id) => id.ToString();
     }
 }
