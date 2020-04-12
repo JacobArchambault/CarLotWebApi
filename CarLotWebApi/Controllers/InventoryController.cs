95192585bf1cd065c7a21a796a59465fe6f59c8d
Jacob Archambault <jacob.archambault@waystar.com>
2020-04-11 23:15:16 -0400

    Add Inventory Controller to CarLotWebApi project

---
+++ CarLotWebApi/Controllers/InventoryController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using AutoLotDAL.Models;
+using AutoLotDAL.Repos;
+using AutoMapper;
+namespace CarLotWebApi.Controllers
+{
+    public class InventoryController : ApiController
+    {
+    }
+}
