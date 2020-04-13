56af9d30ec72e4c1fffcdca9bdf19a55604a1691
Jacob Archambault <jacob.archambault@waystar.com>
2020-04-12 22:28:05 -0400

    Add Database initializer to application start.

--- CarLotWebApi/Global.asax.cs
+++ CarLotWebApi/Global.asax.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using AutoLotDAL.EF;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
@@ -11,6 +13,7 @@ namespace CarLotWebApi
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new MyDataInitializer());
             GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
